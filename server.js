6047e74299d667deb8ca0e834a3d1d8b5bfa2c2c
Willa Jin <jin.willa@gmail.com>
2019-02-07 21:00:45 -0800

    worked on server.js file

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 // dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 // set up express app
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+// express middleware to access public files
+app.use(express.static(path.join(__dirname, "./app/public")));
+
+// express middleware to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// routes
+require("./app/routing/htmlRoutes")(app);
+// var html = require("./app/routing/htmlRoutes");
+// html(app);
+require("./app/routing/apiRoutes")(app);
+// var api = require("./app/routing/apiRoutes");
+// api(app);
+
+// server listen
+app.listen(PORT, function() {
+    console.log("FriendFinder App is listening on PORT " + PORT);
+});
